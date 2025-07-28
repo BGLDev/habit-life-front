d674810993207cfd9a4d16222c20868206d603b3
bgonzalezBio <bgonzalez@biolanmb.com>
2025-07-28 22:24:25 +0200

    initial libs and my-lugin

--- tools/my-plugin/src/generators/my-generator.ts
+++ tools/my-plugin/src/generators/my-generator.ts
@@ -1,9 +1,9 @@
 import {
-  addProjectConfiguration,
+  Tree,
   formatFiles,
   generateFiles,
-  Tree,
 } from '@nx/devkit';
+import { libraryGenerator } from '@nx/angular/generators'; // Asegúrate que esté instalado
 import * as path from 'path';
 import { MyGeneratorGeneratorSchema } from './schema';
 
@@ -11,14 +11,18 @@ export async function myGeneratorGenerator(
   tree: Tree,
   options: MyGeneratorGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  // Ejecuta el generador de librería Angular con los parámetros necesarios
+  await libraryGenerator(tree, {
+    name: options.name,
+    directory: options.name, // <-- Esta es la propiedad requerida
+    standalone: false,
+    changeDetection: 'Default',
+    style: 'scss',
   });
+
+  const projectRoot = `libs/${options.name}`;
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
   await formatFiles(tree);
 }
 
